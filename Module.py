d8db7615b81443782312e6d11cf2e9efa99cc458
Treemo <treemo@hotmail.fr>
2015-12-03 18:28:20 +0100

    add image and sound parameters

--- Module.py
+++ Module.py
@@ -1,12 +1,12 @@
+import os
+
 from core import settings
 from helpers.modules.NotificationModule import NotificationModule
-
-import os
 
 
 class Module(NotificationModule):
     def is_available(self):
         return os.system('notify-send --help > /dev/null') == 0
 
-    def send(self, msg):
+    def send(self, msg, image=None, sound=None):
         os.system('notify-send "%s" "%s" &' % (settings.NAME, msg))
